6cb6e07fc9b8df7ae91718f81a12cda60180392f
Dave Puchyr <dave@slim.avaritia.com>
2021-01-07 15:24:21 +0100

    Use getPagination() and getPageResponse() with domain accounts query

--- x/starname/keeper/querier.go
+++ x/starname/keeper/querier.go
@@ -80,7 +80,6 @@ func queryDomain(ctx sdk.Context, name string, keeper *Keeper) (*types.QueryDoma
 	if err := keeper.DomainStore(ctx).Read(filter.PrimaryKey(), domain); err != nil {
 		return nil, sdkerrors.Wrapf(types.ErrDomainDoesNotExist, "not found: %s", name)
 	}
-
 	return &types.QueryDomainResponse{Domain: domain}, nil
 }
 
@@ -88,32 +87,21 @@ func (q grpcQuerier) DomainAccounts(c context.Context, req *types.QueryDomainAcc
 	if req.Domain == "" {
 		return nil, sdkerrors.Wrapf(types.ErrInvalidDomainName, "'%s'", req.Domain)
 	}
-
-	start := defaultStart
-	end := start + defaultLimit
-	count := false
-	if req.Pagination != nil {
-		if req.Pagination.Key != nil {
-			return nil, sdkerrors.Wrapf(types.ErrInvalidRequest, "pagination by key is not implemented")
-		}
-		start = req.Pagination.GetOffset()
-		limit := req.Pagination.GetLimit()
-		if limit == 0 {
-			limit = defaultLimit
-		}
-		end = start + limit
-		count = req.Pagination.GetCountTotal()
+	start, end, count, err := getPagination(req.Pagination)
+	if err != nil {
+		return nil, sdkerrors.Wrap(err, "failed pagination")
 	}
-
 	return queryDomainAccounts(sdk.UnwrapSDKContext(c), q.keeper, req.Domain, start, end, count)
 }
 
 func queryDomainAccounts(ctx sdk.Context, keeper *Keeper, domain string, start, end uint64, count bool) (*types.QueryDomainAccountsResponse, error) {
-	cursor, err := keeper.AccountStore(ctx).Query().Where().Index(types.AccountDomainIndex).Equals([]byte(domain)).WithRange().Start(start).End(end).Do()
+	query := func() crud.FinalizedIndexStatement {
+		return keeper.AccountStore(ctx).Query().Where().Index(types.AccountDomainIndex).Equals([]byte(domain))
+	}
+	cursor, err := query().WithRange().Start(start).End(end).Do()
 	if err != nil {
 		return nil, sdkerrors.Wrapf(types.ErrInvalidDomainName, "'%s'", domain)
 	}
-
 	accounts := make([]*types.Account, 0)
 	for ; cursor.Valid(); cursor.Next() {
 		account := new(types.Account)
@@ -122,20 +110,10 @@ func queryDomainAccounts(ctx sdk.Context, keeper *Keeper, domain string, start, 
 		}
 		accounts = append(accounts, account)
 	}
-
-	var page *query.PageResponse
-	if count {
-		cursor, err := keeper.AccountStore(ctx).Query().Where().Index(types.AccountDomainIndex).Equals([]byte(domain)).Do()
-		if err != nil {
-			return nil, sdkerrors.Wrapf(types.ErrInvalidDomainName, "'%s'", domain)
-		}
-
-		page = new(query.PageResponse)
-		for ; cursor.Valid(); cursor.Next() {
-			page.Total++
-		}
+	page, err := getPageResponse(count, query())
+	if err != nil {
+		return nil, sdkerrors.Wrapf(err, "'%s' caused error", domain)
 	}
-
 	return &types.QueryDomainAccountsResponse{Accounts: accounts, Page: page}, nil
 }
 
@@ -169,7 +147,10 @@ func (q grpcQuerier) OwnerAccounts(c context.Context, req *types.QueryOwnerAccou
 }
 
 func queryOwnerAccounts(ctx sdk.Context, keeper *Keeper, owner sdk.AccAddress, start, end uint64, count bool) (*types.QueryOwnerAccountsResponse, error) {
-	cursor, err := keeper.AccountStore(ctx).Query().Where().Index(types.AccountAdminIndex).Equals(owner).WithRange().Start(start).End(end).Do()
+	query := func() crud.FinalizedIndexStatement {
+		return keeper.AccountStore(ctx).Query().Where().Index(types.AccountAdminIndex).Equals(owner)
+	}
+	cursor, err := query().WithRange().Start(start).End(end).Do()
 	if err != nil {
 		return nil, sdkerrors.Wrapf(err, "'%s' caused error", bech32FromBytes(owner))
 	}
@@ -181,7 +162,7 @@ func queryOwnerAccounts(ctx sdk.Context, keeper *Keeper, owner sdk.AccAddress, s
 		}
 		accounts = append(accounts, account)
 	}
-	page, err := getPageResponse(count, keeper.AccountStore(ctx).Query().Where().Index(types.AccountAdminIndex).Equals(owner))
+	page, err := getPageResponse(count, query())
 	if err != nil {
 		return nil, sdkerrors.Wrapf(err, "'%s' caused error", bech32FromBytes(owner))
 	}
