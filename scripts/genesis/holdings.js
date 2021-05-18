70ec6291d06ef7af0b8e9bf39e522402a75bb2b1
Dave Puchyr <dave@slim.avaritia.com>
2021-05-18 19:01:18 +0200

    Add unbondings to holdings.js

--- scripts/genesis/holdings.js
+++ scripts/genesis/holdings.js
@@ -11,6 +11,13 @@ const main = async () => {
       o[delegation.delegator_address] = Number( delegation.shares );
       return o;
    }, {} );
+   const unbondings = state.app_state.staking.unbonding_delegations.reduce( ( o, delegation ) => {
+      o[delegation.delegator_address] = delegation.entries.reduce( ( sum, entry ) => {
+         sum += Number( entry.balance );
+         return sum;
+      }, 0 );
+      return o;
+   }, {} );
    const star12special = Object.keys( multisigs ).reduce( ( o, k ) => {
       o[multisigs[k].star1] = multisigs[k]["//name"];
       return o;
@@ -25,14 +32,15 @@ const main = async () => {
       return names.map( name => `${name.name}*${name.domain}` ).join( " | " );
    };
 
-   console.log( [ "star1", "total", "available", "delegated", "name(s)" ].join( "," ) );
+   console.log( [ "star1", "total", "available", "delegated", "unbonding", "name(s)" ].join( "," ) );
    availables.forEach( account => {
       const star1 = account.value.address;
       const name = star12special[star1] || getStarnames( star1 );
       const available = account.value.coins[0].amount / 1e6;
       const delegated = ( delegations[star1] || 0 ) / 1e6;
-      const total = available + delegated;
-      console.log( [ account.value.address, total, available, delegated, name ].join( "," ) );
+      const unbonding = ( unbondings[star1] || 0 ) / 1e6;
+      const total = available + delegated + unbonding;
+      console.log( [ account.value.address, total, available, delegated, unbonding, name ].join( "," ) );
    } );
 }
 
