26f64805cbc75557fb6ece6ff9900f80f53fac27
Dave Puchyr <dave@slim.avaritia.com>
2021-06-08 15:58:36 +0200

    Revert debugging help

--- scripts/genesis/lib/migrate.js
+++ scripts/genesis/lib/migrate.js
@@ -567,8 +567,7 @@ export const migrate = async args => {
          resolve( out );
 
          // clean-up superflous files
-         // dmjp [ "addrbook.json", "app.toml", "config.toml", "launchpad.json", "node_key.json", "priv_validator_key.json" ].map( f => path.join( config, f ) ).forEach( f => { if ( fs.existsSync( f ) ) fs.unlinkSync( f ) } );
-         [ "addrbook.json", "app.toml", "config.toml", "node_key.json" ].map( f => path.join( config, f ) ).forEach( f => { if ( fs.existsSync( f ) ) fs.unlinkSync( f ) } );
+         [ "addrbook.json", "app.toml", "config.toml", "launchpad.json", "node_key.json", "priv_validator_key.json" ].map( f => path.join( config, f ) ).forEach( f => { if ( fs.existsSync( f ) ) fs.unlinkSync( f ) } );
          [ "data", "wasm" ].map( dir => path.join( home, dir ) ).forEach( dir => { if ( fs.existsSync( dir ) ) fs.rmdirSync( dir, { recursive: true } ) } );
          fs.readdirSync( config ).filter( f => f.indexOf( "write-file-atomic" ) == 0 ).forEach( f => fs.unlinkSync( path.join( config, f ) ) );
       } );
