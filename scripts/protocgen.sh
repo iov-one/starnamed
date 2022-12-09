2dd19b6c2d826cc06e855f53bde4b6cc87040480
Davi Mello <dsmello.9@gmail.com>
2022-12-09 19:35:12 +0000

    EDIT.: fix the scripts to generate the API docs - fix scripts - update statck

--- scripts/protocgen.sh
+++ scripts/protocgen.sh
@@ -1,35 +1,32 @@
-#!/usr/bin/env bash
 
 set -eo pipefail
 
-# get protoc executions
-go get github.com/regen-network/cosmos-proto/protoc-gen-gocosmos 2>/dev/null
-
-# get cosmos sdk from github
-go get github.com/cosmos/cosmos-sdk@v0.45.9 2>/dev/null
+protoc_install_proto_gen_doc() {
+  echo "Installing protobuf protoc-gen-doc plugin"
+  (go install github.com/pseudomuto/protoc-gen-doc/cmd/protoc-gen-doc@latest 2> /dev/null)
+}
 
 echo "Generating gogo proto code"
 cd proto
-proto_dirs=$(find ./iov ./cosmwasm -path -prune -o -name '*.proto' -print0 | xargs -0 -n1 dirname | sort | uniq)
+proto_dirs=$(find ./cosmwasm ./iov -path -prune -o -name '*.proto' -print0 | xargs -0 -n1 dirname | sort | uniq)
 for dir in $proto_dirs; do
   for file in $(find "${dir}" -maxdepth 1 -name '*.proto'); do
-    if grep go_package $file &>/dev/null; then
+    if grep "option go_package" $file &> /dev/null ; then
       buf generate --template buf.gen.gogo.yaml $file
     fi
   done
 done
 
+protoc_install_proto_gen_doc
+
+echo "Generating proto docs"
+buf generate --template buf.gen.doc.yaml
+
 cd ..
 
 # move proto files to the right places
-#
-# Note: Proto files are suffixed with the current binary version.
 cp -r github.com/CosmWasm/wasmd/* ./
 cp -r github.com/iov-one/starnamed/* ./
 rm -rf github.com
 
-go mod tidy -compat=1.18
-
-# TODO: Uncomment once ORM/Pulsar support is needed.
-#
-# Ref: https://github.com/osmosis-labs/osmosis/pull/1589
+go mod tidy
