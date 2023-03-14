e1a955c20b6769eee2ab91b7f9446e0e3ff78d8d
Davi Mello <dsmello.9@gmail.com>
2023-03-14 17:16:54 +0000

    review in the future

--- scripts/protoc-swagger-gen.sh
+++ scripts/protoc-swagger-gen.sh
@@ -7,6 +7,8 @@ WORKDIR=$(pwd)
 mkdir -p ./tmp-swagger-gen
 proto_dirs=$(find ./proto -path -prune -o -name '*.proto' -print0 | xargs -0 -n1 dirname | sort | uniq)
 for dir in $proto_dirs; do
+  dir_temp=./tmp-swagger-gen/$(echo $dir|  sed  's/\.\///g' | sed 's/\//-/g')
+  mkdir -p $dir_temp
 
   # generate swagger files (filter query files)
   query_file=$(find "${dir}" -maxdepth 1 -name 'query.proto')
@@ -15,8 +17,7 @@ for dir in $proto_dirs; do
     -I "proto" \
     -I "third_party/proto" \
     "$query_file" \
-    --swagger_out ./tmp-swagger-gen \
-    --swagger_opt logtostderr=true --swagger_opt fqn_for_swagger_name=true --swagger_opt simple_operation_ids=true
+    --openapi_out $dir_temp
   fi
 done
 
@@ -34,3 +35,4 @@ scripts/fetch-cosmos-swagger.sh $WORKDIR
 
 # Update static assets
 statik -src client/docs/swagger-ui/ -dest client/docs/ -f -m
+
